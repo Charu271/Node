8f8874b5c9f1ff4d9718a1b3a488d7acf5c1fba3
Charu <charu0012.cse19@chitkara.edu.in>
2020-10-21 23:40:02 +0530

    section 4

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,9 +1,9 @@
 const request = require("request");
 const url =
-  "http://api.weatherstack.com/current?access_key=6849cc02b25192ee34e0ef95aceeadde&query=New%20York";
-request({ url: url }, function (error, response) {
-  const obj = JSON.parse(response.body).current;
+  "http://api.weatherstack.com/current?access_key=6849cc02b25192ee34e0ef95aceeadde&query=New%20York&units=f";
+request({ url: url, json: true }, function (error, response) {
+  const obj = response.body.current;
   console.log(
-    `The current temprature is ${obj.temperature} and feel like temperature is ${obj.feelslike}`
+    `${obj.weather_descriptions[0]} The current temprature is ${obj.temperature} and feel like temperature is ${obj.feelslike}`
   );
 });
